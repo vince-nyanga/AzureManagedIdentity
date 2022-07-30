cf41b3b71031d60c32b69e2753d3663ab40b1b51
Vincent Nyanga <vince.nyanga@hotmail.com>
2022-07-30 07:55:52 +0200

    added project

---
+++ src/ManagedIdentitySample/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.Versioning;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+builder.Services.AddControllers();
+builder.Services.AddApiVersioning(options =>
+{
+    options.ApiVersionReader = new UrlSegmentApiVersionReader();
+    options.ReportApiVersions = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+});
+
+builder.Services.AddVersionedApiExplorer(option =>
+{
+    option.GroupNameFormat = "'v'V";
+    option.SubstituteApiVersionInUrl = true;
+    option.AssumeDefaultVersionWhenUnspecified = true;
+});
+builder.Services.AddEndpointsApiExplorer();
+var versionDescriptionProvider = builder.Services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
+
+foreach (var description in versionDescriptionProvider.ApiVersionDescriptions)
+{
+    builder.Services.AddSwaggerDocument(document =>
+    {
+        document.DocumentName = description.GroupName;
+        document.Version = description.GroupName;
+    });
+}
+
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi3();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
